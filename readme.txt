c4f0f7bc2af9601f9fd69110dbadd599baf3eb1a
mikethorpe <michael.a.thorpe@gmail.com>
2018-11-10 23:47:00 +0000

    Adding pdf version of evidence gathering checklist. Refactored files and folders.

--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
-Professional Development Award repo
+Professional Development Award repo:
+Files for submission include:
+/Static_and_Dynamic_Task_A
+/Unit_and_Integration_Task_B
+./mike_thorpe_evidence_gathering_checklist_pda.pdf
---
+++ source/code/ruby/code.rb
@@ -0,0 +1,43 @@
+def containsThree?(array)
+    
+    result = array.index(3)
+
+    if result != nil
+        puts "Found 3"
+        return;
+    end
+
+    puts "3 is not in the array"
+end
+
+def sortNumbersAscending(array_of_numbers)
+    sorted_numbers = array_of_numbers.sort()
+    "Puts sorted numbers ascending"
+    puts sorted_numbers
+    return sorted_numbers
+end
+
+array1 = [2, 546, 7, 8, 6, 7]
+
+sortNumbersAscending(array1)
+
+
+
+def copyArrayPlusOne(array)
+    # create a new empty array
+    # loop over all elements in array passed
+    # add one to the element
+    # store the element in the new array as a new element
+    # return the new array 
+end
+
+
+
+
+
+
+
+
+
+
+array2 = [ 5, 6, 1, 3, 9]
